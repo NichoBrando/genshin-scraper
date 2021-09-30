870716229284d348a6dacee26d2df64fc84f92d1
Nicholas Brandão <nicholas.computacao@outlook.com>
2021-09-29 23:17:23 -0300

    Refactored code

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const { Builder, By } = require('selenium-webdriver');
 const fs = require('fs');
+const Promise = require('bluebird');
 
 const generateJSON = async() => {
   const driver = await new Builder().forBrowser('firefox').build();
@@ -17,11 +18,25 @@ const generateJSON = async() => {
   }
 
   try {
-    await driver.get('https://genshin.gg/');
-    const characters = await getList('character-icon');
-    await driver.get('https://genshin.gg/weapons');
-    const weapons = await getList('table-image');
-    const itemsData = [...characters, ...weapons];
+    const targets = [
+      {
+        url: 'https://genshin.gg/',
+        className: 'character-icon'
+      },
+      {
+        url: 'https://genshin.gg/weapons',
+        className: 'table-image'
+      }
+    ];
+    const itemsData = [];
+    await Promise.each(
+      targets, 
+      async target => {
+        await driver.get(target.url);
+        const newItems = await getList(target.className);
+        itemsData.push(...newItems);
+      }
+    );
     fs.writeFile('items.json', JSON.stringify(itemsData), 'utf8', () => 1);
   } finally {
     await driver.quit();
